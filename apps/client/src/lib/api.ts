d3f0b57db166ec47b01fc4a0ecdb9a6c781a3b74
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2025-04-18 17:16:24 -0300

    fix: missing variable in client

--- apps/client/src/lib/api.ts
+++ apps/client/src/lib/api.ts
@@ -1,5 +1,9 @@
+"use server"
+
 export const fetchSentence = async () => {
-  const res = await fetch("http://localhost:5001/api/sentence?count=5")
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/sentence?count=5`
+  )
   if (!res.ok) throw new Error("Failed to fetch sentence")
   return res.json()
 }
